e09974492a6542075bb2f20c6983ee732235c4f1
Luis Diniz <dinnizluis@gmail.com>
2015-11-22 15:33:18 -0500

    HeadCount updated

--- ACM_SIGCSE/app/src/main/java/mobilecomp/acm_sigcse/HeadCount.java
+++ ACM_SIGCSE/app/src/main/java/mobilecomp/acm_sigcse/HeadCount.java
@@ -12,35 +12,46 @@ public class HeadCount {
     private int headCount;
     private String timeAdded;
 
+    //Returns the ID for each head count taken
     public int getHeadCountID()
     {
        return headCountID;
     }
+
+    //Returns the ID for an activity
     public int getActivityID()
     {
         return activityID;
     }
+
+    //Returns the overall headcount
     public int getHeadCount()
     {
         return headCount;
     }
+
+    //Returns the time a headcount was added
     public String getTimeAdded()
     {
         return timeAdded;
     }
 
+    //Sets the headcount ID to an int
     public void setHeadCountID(int i)
     {
         headCountID = i;
     }
+    //Sets the headcount ID to an int
     public void setActivityID(int i)
     {
         activityID = i;
     }
+    //Sets the activity ID to an int
     public void setHeadCount(int i)
     {
         headCount = i;
     }
+    //Sets the time a headcount was added
     public void setTimeAdded(String d)
     {
         timeAdded = d;
